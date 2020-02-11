8bc22d3de229a347a5890e04b010c0fdaf6a0998
Vivek Shwarup <32225812+vivekshwarup@users.noreply.github.com>
2020-02-11 15:44:41 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+apt -y install libpcre3 libpcre3-dev build-essential autoconf automake libtool libpcap-dev libnet1-dev libyaml-0-2 libyaml-dev zlib1g zlib1g-dev libmagic-dev libcap-ng-dev libjansson-dev pkg-config libnetfilter-queue-dev geoip-bin geoip-database geoipupdate apt-transport-https
+add-apt-repository ppa:oisf/suricata-stable
+apt-get update
