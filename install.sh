ba68b9ca1cf1be0e0c205a52795a81d11faad761
Vivek Shwarup <32225812+vivekshwarup@users.noreply.github.com>
2020-02-11 16:03:09 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 apt-get update -y
 apt -y install libpcre3 libpcre3-dev build-essential autoconf automake libtool libpcap-dev libnet1-dev libyaml-0-2 libyaml-dev zlib1g zlib1g-dev libmagic-dev libcap-ng-dev libjansson-dev pkg-config libnetfilter-queue-dev geoip-bin geoip-database geoipupdate apt-transport-https
+echo -e "\n\e[33m Add suricata repository\e[0m"
 add-apt-repository ppa:oisf/suricata-stable
-apt-get update -y
-apt-get install suricata -y
+apt-get update
+ip link set ens160 promisc on
+apt install python-pip
+pip install pyyaml
+pip install https://github.com/OISF/suricata-update/archive/master.zip
+pip install --pre --upgrade suricata-update
+suricata-update
+suricata-update update-sources
+suricata-update enable-source ptresearch/attackdetection
+suricata-update enable-source oisf/trafficid
+suricata-update enable-source sslbl/ssl-fp-blacklist
+suricata-update
