e403260cb47d2b7b44aefb8661f5c045b8ee94f7
Vivek Shwarup <32225812+vivekshwarup@users.noreply.github.com>
2020-02-11 15:52:50 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
-#!/usr/bin/env bash
+#!/bin/bash
+apt-get update -y
 apt -y install libpcre3 libpcre3-dev build-essential autoconf automake libtool libpcap-dev libnet1-dev libyaml-0-2 libyaml-dev zlib1g zlib1g-dev libmagic-dev libcap-ng-dev libjansson-dev pkg-config libnetfilter-queue-dev geoip-bin geoip-database geoipupdate apt-transport-https
 add-apt-repository ppa:oisf/suricata-stable
-apt-get update
+apt-get update -y
+apt-get install suricata -y
